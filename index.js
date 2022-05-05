b8a731d92008257b122f1105b9c7afa3c03a2abe
lelaranja <leticiajlaranja@gmail.com>
2022-05-05 10:05:10 -0300

    Implementação base64 e ajuste responsividade

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var codigo = document.querySelector("#digCod").value;
 var selecao = document.getElementById("selecao");
 var itemCC = document.getElementById("CCesar1");
 var divPai = document.querySelector('#pai');
 var inCod = document.querySelector('#inCode');
-var deCod = document.querySelector('#deCode')
-var btnRodar = document.querySelector('#btnRoda')
-var radioBtn = document.querySelector('#radio')
-var resultado = document.querySelector('#resultadoCod')
+var deCod = document.querySelector('#deCode');
+var btnRodar = document.querySelector('#btnRoda');
+var radioBtn = document.querySelector('#radio');
+var codigoInicial = document.querySelector("#digCod").value;
+var passo = document.querySelector("#passoCCesar");
 
 //Mudar layout - Seleção Cifra de César
 selecao.addEventListener('click', function () {
@@ -16,32 +16,98 @@ selecao.addEventListener('click', function () {
         itemCC.style.justifyContent = 'center'
         itemCC.style.alignItems = 'center'
         itemCC.style.paddingTop = '0.5em'
-        divPai.style.paddingBottom = '1em'
+        divPai.style.paddingBottom = '2.5em'
     } else {
         itemCC.style.display = 'none'
         divPai.style.paddingBottom = '10em'
     }
 })
 
-//Mudar texto do botão de rodar
+//Mudar msgo do botão de rodar
 inCod.addEventListener('click', function () {
-    btnRodar.innerText = 'Codificar mensagem'
+    btnRodar.innerText = 'Codificar mensagem';
 })
 
 deCod.addEventListener('click', function () {
-    btnRodar.innerText = 'Decodificar mensagem'
+    btnRodar.innerText = 'Decodificar mensagem';
 })
 
-//Rodar Base64
-btnRodar.addEventListener('click', function () {
-    if (selecao.value == '2' && inCod.click) {
-       // var resposta = btoa(codigo)
-        resultado.value = btoa(codigo)
-    } else if (selecao.value == '2' && deCod.click) {
-    //  var resposta = atob(codigo)
-        resultado.value = atob(codigo)
-    }else{
-        alert('Realize a seleção corretamente')
+// Função para de/codificação - Cifra Cesar
+
+function codificaCesar(codigoInicial, passo) {
+    var saidaTexto = '';
+    var nTroca = 0;
+    var resultado = document.querySelector("#resultadoCod");
+    var codigoInicial = document.querySelector("#digCod").value;
+    for (var i = 0; i < codigoInicial.length; i++) {
+        if ((codigoInicial.charCodeAt(i) >= 65 && codigoInicial.charCodeAt(i) <= 90)) {
+            nTroca = (Number(codigoInicial.charCodeAt(i))) - 65;
+            nTroca = (nTroca + (Number(passo))) % 26;
+            nTroca += 65;
+            saidaTexto += String.fromCharCode(nTroca);
+        } else if (codigoInicial.charCodeAt(i) >= 97 && codigoInicial.charCodeAt(i) <= 122) {
+            nTroca = (Number(codigoInicial.charCodeAt(i))) - 97;
+            nTroca = (nTroca + (Number(passo))) % 26;
+            nTroca += 97;
+            saidaTexto += String.fromCharCode(nTroca);
+        } else {
+            saidaTexto += String.fromCharCode(codigoInicial.charCodeAt(i));
+        }
+    }
+    resultado.innerText = saidaTexto;
+}
+
+function decodificaCesar(codigoInicial, passo) {
+    var saidaTexto = '';
+    var nTroca = 0;
+    var resultado = document.querySelector("#resultadoCod");
+    var codigoInicial = document.querySelector("#digCod").value;
+    for (var i = 0; i < codigoInicial.length; i++) {
+        if ((codigoInicial.charCodeAt(i) >= 65 && codigoInicial.charCodeAt(i) <= 90)) {
+            nTroca = (Number(codigoInicial.charCodeAt(i))) - 65;
+            nTroca = (nTroca - (Number(passo - 26))) % 26;
+            nTroca += 65;
+            saidaTexto += String.fromCharCode(nTroca);
+        } else if (codigoInicial.charCodeAt(i) >= 97 && codigoInicial.charCodeAt(i) <= 122) {
+            nTroca = (Number(codigoInicial.charCodeAt(i))) - 97;
+            nTroca = (nTroca - (Number(passo - 26))) % 26;
+            nTroca += 97;
+            saidaTexto += String.fromCharCode(nTroca);
+        } else {
+            saidaTexto += String.fromCharCode(codigoInicial.charCodeAt(i));
+        }
+    }
+    resultado.innerText = saidaTexto;
+}
+
+// Função para de/codificação - Base 64
+function codificaBase64() {
+    var codigoInicial = document.querySelector("#digCod").value
+
+    if (document.querySelector('#inCode').checked && selecao.value == '2') {
+        var saidaTexto = btoa(codigoInicial);
+        document.querySelector('#resultadoCod').innerText = saidaTexto;
+    } else if (document.querySelector('#deCode').checked && selecao.value == '2') {
+        var saidaTexto = atob(codigoInicial);
+        document.querySelector('#resultadoCod').innerText = saidaTexto;
+    }
+}
+
+//Rodar Base64 e Cifra de César
+btnRodar.addEventListener('click', function (e) {
+    e.preventDefault();
+    var codigoInicial = document.querySelector("#digCod").value;
+
+    if (selecao.value == '2' && inCod.checked) {
+        codificaBase64(codigoInicial)
+    } else if (selecao.value == '2' && deCod.checked) {
+        codificaBase64(codigoInicial)
+    } else if (selecao.value == '3' && inCod.checked) {
+        codificaCesar(codigoInicial, passo)
+    } else if (selecao.value == '3' && deCod.checked) {
+        decodificaCesar(codigoInicial, passo)
+    } else {
+        alert('Preencha todas as informações corretamente')
     }
 }
 )
